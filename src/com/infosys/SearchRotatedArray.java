23f992d717c940cd267f631b52f80bcfad450bca
proab <proabhu@gmail.com>
2022-08-25 15:12:35 -0400

    from windows

--- src/com/infosys/SearchRotatedArray.java
+++ src/com/infosys/SearchRotatedArray.java
@@ -2,36 +2,30 @@ package com.infosys;
 
 public class SearchRotatedArray {
     public static void main(String[] args) {
-        int [] nums = {4,5,6,7, 8 , 10, 0,1,2};
+        int[] nums = { 4,5,6,7,0,1,2};
         System.out.println(highestIndex(nums));
     }
-    static int search(int[] nums, int target) {
-        int answer = -1;
-        return 0;
-    }
 
+    static int highestIndex(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
 
+        while (start <= end) {
+            int mid = (start + end)/2;
 
-    static int highestIndex(int[] nums){
-        int start =0;
-        int end = nums.length -1;
-
-        while(start < end) {
-            int mid = (start+end)/2;
-            if (nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1] ){
-                return mid;
+            if (arr[mid] > arr[mid +1]){
+            return mid;
             }
-            if(nums[mid] > nums[mid +1]){
-                end = mid;
+            if (arr[mid] > arr[start]){
+             start = mid+1;
             }else{
-                start = mid;
+                end = mid-1;
             }
+
         }
-        return start;
-
+        return end;
     }
+    static int binarySearch(int [] nums, int target, int start, int end){
 
-    static  int leftSearch(int [] nums){
-        return 0;
     }
 }
